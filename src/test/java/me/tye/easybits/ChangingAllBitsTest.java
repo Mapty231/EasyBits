671cf1a1c7dacd96028ccd09e64f95e2a124235d
tye <tye@mailbox.org>
2024-03-14 01:16:26 +0000

    Added tests for method that modify the entire BitHolder
    Now tested:
    - BitHolder.allHigh
    - BitHolder.allLow
    - BitHolder.areAllHigh
    - BitHolder.areAllLow

--- src/test/java/me/tye/easybits/ChangingAllBitsTest.java
+++ src/test/java/me/tye/easybits/ChangingAllBitsTest.java
@@ -6,6 +6,14 @@ import org.junit.jupiter.api.Test;
 public class ChangingAllBitsTest {
 
 @Test
+public void allLow() {
+  BitHolder bits = BitHolder.fromBitString("01011010 11110000");
+  bits.allLow();
+  Assertions.assertEquals(BitHolder.fromBitString("00000000 00000000"), bits);
+  Assertions.assertTrue(bits.areAllLow());
+}
+
+@Test
 public void allLowEmpty() {
   BitHolder bits = BitHolder.fromBitString("");
   bits.allLow();
@@ -13,4 +21,18 @@ public void allLowEmpty() {
 }
 
 
+@Test
+public void allHigh() {
+  BitHolder bits = BitHolder.fromBitString("01011010 11110000");
+  bits.allHigh();
+  Assertions.assertEquals(BitHolder.fromBitString("11111111 11111111"), bits);
+  Assertions.assertTrue(bits.areAllHigh());
+}
+
+@Test
+public void allHighEmpty() {
+  BitHolder bits = BitHolder.fromBitString("");
+  bits.allLow();
+  Assertions.assertEquals(BitHolder.fromBitString(""), bits);
+}
 }
